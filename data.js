f4bebbbcfaa0e74a6d885de7feed2817e1c880f4
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2026-07-03 12:44:00 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overview1",
+      "name": "OVERVIEW1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.41218639509209964,
+        "pitch": 0.13210355874226032,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8510210802003684,
+          "pitch": 0.09489350058852786,
+          "rotation": 0,
+          "target": "1-overview2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-overview2",
+      "name": "OVERVIEW2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.8522453876907221,
+        "pitch": 0.4849634798850371,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7817459652815213,
+          "pitch": 0.2575422326509802,
+          "rotation": 0,
+          "target": "0-overview1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SF-13_01.07.2026",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
